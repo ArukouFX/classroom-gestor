03fb94e46c2c4ce378f9da9df447af7d8d94c970
ArukouFX <felipecarballowk@gmail.com>
2025-03-29 15:24:06 -0300

    Removing Spring security

--- aula-backend/src/main/java/com/aula/security/SecurityConfig.java
+++ aula-backend/src/main/java/com/aula/security/SecurityConfig.java
@@ -2,12 +2,12 @@ package com.aula.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
+/*
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -28,5 +28,5 @@ public class SecurityConfig {
                 }));
 
         return http.build();
-    }
+    }*/
 }
